19ca311b29b1c9f4750bd7ac356c2ace3820eaec
jaesoek <ljs981120@gmail.com>
2022-03-28 22:14:59 +0900

    Add 팰린드롬

--- etc/Sorting/삽입정렬.js
+++ etc/Sorting/삽입정렬.js
@@ -1,6 +1,7 @@
 function solution(arr) {
   for (let i = 1; i < arr.length; i++) {
-    let temp = arr[i];
+    let temp = arr[i],
+      j;
     for (j = i - 1; j >= 0; j--) {
       if (arr[j] > temp) arr[j + 1] = arr[j];
       else break;
---
+++ 백준/String/팰린드롬.py
@@ -0,0 +1,27 @@
+from typing import Counter
+
+name = list(input())
+name.sort()
+
+length = len(name)
+
+l1 = []
+l2 = []
+l3 = []
+
+counter = Counter(name)
+
+for key, value in counter.items():
+  for _ in range(value // 2):
+    l1.append(key)
+    l2.append(key)
+  
+  for _ in range(value%2):
+    l3.append(key)
+
+if len(l3) > 1 or (not length%2 and l3):
+  print("I'm Sorry Hansoo")
+else:
+  print(''.join(l1+l3+l2[::-1]))
+
+
