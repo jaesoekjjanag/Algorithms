3320716f121d956124dba8b7def9606afc19fec1
jaesoek <ljs981120@gmail.com>
2022-11-21 23:47:40 +0900

    프로그래머스: 정수삼각형

---
+++ 백준/DP/신기한소수.py
@@ -0,0 +1,30 @@
+def dfs(dp, n):
+  if dp[int(n)] == 1:
+    return False
+  
+  length = len(n)
+  if length == 1 and dp[int(n)] == 0:
+    return True
+  
+  return dfs(dp, n[:length-1])
+
+def solution(N):
+  max_num = 10**N
+  dp = [0] * (max_num + 1)
+  dp[0] = 1
+  dp[1] = 1
+  
+  for i in range(2, max_num):
+    s = 2
+    while True:
+      crnt = i*s
+      if crnt > max_num:
+        break
+      dp[crnt] = 1
+      s += 1
+  
+  for i in range(int(max_num/10), max_num):
+    # if dfs(dp, str(i)):
+    print(i)
+  
+solution(8)
