bec86a9325bc3b932c542dd95d699062f0cc0e57
jaesoek <ljs981120@gmail.com>
2022-11-24 13:02:36 +0900

    백준: 괄호의 값-2504

---
+++ 백준/Stack/괄호의 값/괄호의 값.py
@@ -0,0 +1,43 @@
+# (): 2
+# []: 3
+# (x): 2*val(x)
+# [x]: 3*val(x)
+# xy: val(x) + val(y)
+
+# f = open("괄호의 값.txt")
+import sys
+# braces = f.readline().rstrip()
+braces = sys.stdin.readline().rstrip()
+op = {'(': 2, '[' : 3, ')': 2, ']': 3}
+pair = {')': '(', ']': '['}
+o_br = ['(', '[']
+c_br = [')', ']']
+
+def solution():
+  stack = []
+  total = 0
+  temp = 1
+  last = ''
+  
+  for i in braces:
+    if i in o_br:
+      stack.append(i)
+      temp *= op[i]
+      
+    if i in c_br:
+      if not stack:
+        return 0
+      
+      top = stack.pop()
+      if pair[i] != top:
+        return 0
+
+      if pair[i] == last:
+        total += temp
+      temp //= op[i]
+      
+    last = i
+
+  return total  
+
+print(solution())
