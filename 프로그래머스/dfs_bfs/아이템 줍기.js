87d163a2ecf623fcdb8102a060e11b28857ee24c
jaesoek <ljs981120@gmail.com>
2023-01-19 10:16:10 +0900

    프로그래머스: 아이템 줍기

--- 프로그래머스/dfs_bfs/아이템 줍기.js
+++ 프로그래머스/dfs_bfs/아이템 줍기.js
@@ -7,12 +7,11 @@ function drawLine(start, end, grid) {
     [1, 0],
     [0, 1],
   ];
-  const visited = Array.from({ length: 51 }, () => Array(51).fill(0));
+  const visited = Array.from({ length: 102 }, () => Array(102).fill(0));
   while (q.length) {
     const [x, y] = q.shift();
     visited[x][y] = 1;
-
-    // 1: 모서리, 2: 내부.
+    // 1: 모서리, -1: 내부.
     // 모서리지만 이미 2로 채워져 있다는 것은 다른 도형의 내부라는 뜻. 모서리 초리하지 않는다.
     if (x === sx || x === ex || y === sy || y === ey) {
       if (grid[x][y] !== -1) {
@@ -24,8 +23,7 @@ function drawLine(start, end, grid) {
 
     d.forEach(([dx, dy]) => {
       const [nx, ny] = [x + dx, y + dy];
-
-      if (nx <= ex && ny <= ey && !visited[nx][ny]) {
+      if (nx <= ex && ny <= ey && visited[nx][ny] === 0) {
         q.push([nx, ny]);
       }
     });
@@ -34,62 +32,62 @@ function drawLine(start, end, grid) {
 
 function solution(rectangle, characterX, characterY, itemX, itemY) {
   let answer = 2500;
-  const grid = Array.from({ length: 51 }, () => Array(51).fill(0));
+  const grid = Array.from({ length: 102 }, () => Array(102).fill(0));
+  characterX *= 2;
+  characterY *= 2;
+  itemX *= 2;
+  itemY *= 2;
+
+  rectangle = rectangle.map((r) => r.map((x) => x * 2));
 
   rectangle.forEach(([x1, y1, x2, y2]) => {
-    drawLine([x1, y1], [x2, y2], grid);
+    for (let i = x1; i <= x2; i++) {
+      for (let j = y1; j <= y2; j++) {
+        if (i === x1 || i === x2 || j === y1 || j === y2) {
+          if (grid[i][j] === 0) grid[i][j] = 1;
+        } else {
+          grid[i][j] = -1;
+        }
+      }
+    }
   });
 
-  const directions = {
-    down: [0, -1],
-    left: [-1, 0],
-    right: [1, 0],
-    up: [0, 1],
-  };
+  // for (let i = 30; i > 0; i--) {
+  //   for (let j = 1; j <= 30; j++) {
+  //     if (grid[i][j] === 1) {
+  //       process.stdout.write("*");
+  //     } else {
+  //       process.stdout.write(" ");
+  //     }
+  //   }
+  //   console.log();
+  // }
 
-  const priority = {
-    down: 2,
-    left: 1,
-    right: 1,
-    up: 0,
-  };
+  const directions = [
+    [0, -1],
+    [-1, 0],
+    [1, 0],
+    [0, 1],
+  ];
 
   function dfs(x, y, grid, acc) {
-    console.log(x, y);
     if (x === itemX && y === itemY) {
       answer = Math.min(answer, acc);
       return;
     }
 
-    grid[x][y] = acc;
-
-    const dup = [];
-    Object.entries(directions).forEach(([dir, [dx, dy]]) => {
-      if (
-        !(x + dx === characterX && y + dy === characterY) &&
-        grid[x + dx][y + dy] == 1
-      ) {
-        dup.push(dir);
+    grid[x][y] = 0;
+    for (let [dx, dy] of directions) {
+      const [nx, ny] = [x + dx, y + dy];
+      if (grid[nx][ny] === 1) {
+        dfs(x + dx, y + dy, grid, acc + 1);
       }
-    });
-    if (dup.length === 0) return;
-    const direction = dup.sort((a, b) => priority[b] - priority[a])[0];
-
-    const [dx, dy] = directions[direction];
-
-    dfs(x + dx, y + dy, grid, acc + 1);
-  }
-
-  for (let d of Object.values(directions)) {
-    const [dx, dy] = d;
-
-    if (grid[characterX + dx][characterY + dy] === 1) {
-      console.log();
-      dfs(characterX + dx, characterY + dy, grid, 2);
     }
   }
 
-  return answer - 1;
+  dfs(characterX, characterY, grid, 1);
+
+  return (answer - 1) / 2;
 }
 
 console.log(
@@ -106,17 +104,18 @@ console.log(
     1
   )
 );
-// console.log(
-//   solution(
-//     [
-//       [1, 1, 7, 4],
-//       [3, 2, 5, 5],
-//       [4, 3, 6, 9],
-//       [2, 6, 8, 8],
-//     ],
-//     1,
-//     3,
-//     7,
-//     8
-//   )
-// );
+console.log(
+  solution(
+    [
+      [1, 1, 7, 4],
+      [3, 2, 5, 5],
+      [4, 3, 6, 9],
+      [2, 6, 8, 8],
+    ],
+    1,
+    3,
+    7,
+    8
+  )
+);
+console.log(solution([[1, 1, 5, 7]], 1, 1, 4, 7));
