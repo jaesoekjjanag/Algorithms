dfb55922446cc104f64413aabcfc54a2e75a2b00
jaesoek <ljs981120@gmail.com>
2023-02-03 15:46:04 +0900

    프로그래머스: 도둑질

---
+++ 프로그래머스/dp/도둑질.py
@@ -0,0 +1,20 @@
+def solution(money):
+    dp1 = [0] * len(money)
+    dp2 = dp1[::]
+    
+    # 첫 집을 훔치는 경우
+    dp1[0] = money[0]
+    dp1[1] = max(money[0], money[1])
+
+    # 첫 집을 훔치지 않는 경우
+    dp2[0] = 0
+    dp2[1] = money[1]
+    
+    for i in range(2, len(money)-1):
+        dp1[i] = max(dp1[i-1], money[i]+dp1[i-2])
+
+    for i in range(2, len(money)): 
+        dp2[i] = max(dp2[i-1], money[i]+dp2[i-2])
+
+    return max(max(dp1), max(dp2))
+
