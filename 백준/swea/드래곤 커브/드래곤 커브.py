f8bd08249e7943e9a5ea2881a2d14ae6f0fd279d
jaesoek <ljs981120@gmail.com>
2023-03-29 08:18:15 +0900

    백준: 드래곤 커브-15685

--- 백준/swea/드래곤 커브/드래곤 커브.py
+++ 백준/swea/드래곤 커브/드래곤 커브.py
@@ -1,16 +1,43 @@
 import sys
 
-input = open("./input.txt").readline
-
+# input = sys.stdin.readline
+input = open('./input.txt').readline
 n = int(input())
 
 direction = [
-  [0, 1],
+  [1, 0],
+  [0, -1],
   [-1, 0],
-  [-1, 0],
-  [1, 0]
+  [0, 1]
 ]
+grid = [[0] * 101 for _ in range(101)]
 
-def dragonCurve(y, x, d, n, curve):
-  curve[y][x] = 1
+def dragon_curve(y, x, d, g):
+  grid[y][x] = 1
+  conn = [d]
   
+  for _ in range(g):
+    grid[y][x] = 1
+    
+    for i in range(len(conn)-1, -1, -1):
+      conn.append((conn[i]+1)%4)
+  
+  for i in range(len(conn)):
+    y, x = y+direction[conn[i]][0], x+direction[conn[i]][1]
+    if 0<=y<101 and 0<=x<101:
+        continue
+    grid[y][x] = 1
+
+    
+
+for _ in range(n):
+  y, x, d, g = map(int, input().split())
+  dragon_curve(y, x, d, g)
+  
+answer = 0
+for i in range(100):
+    for j in range(100):
+        if grid[i][j] and grid[i][j + 1] and grid[i + 1][j] and grid[i + 1][j + 1]:
+            answer += 1
+            
+print(answer)
---
+++ 프로그래머스/kakao_blind_2023/택배 배달 수거하기.py
@@ -0,0 +1,34 @@
+def solution(cap, n, deliveries, pickups):
+    answer = 0
+    
+    for p in range(n-1, -1, -1):
+        if(deliveries[p] > 0 or pickups[p] > 0):
+            answer += (p+1)
+            deliver = 0
+            for i in range(p, -1, -1):
+                if(deliveries[i] > 0 and deliver < cap):
+                    if(cap-deliver > deliveries[i]):
+                        deliver += deliveries[i] 
+                        deliveries[i] = 0
+                    else:
+                        deliveries[i] -= (cap-deliver)
+                        deliver = cap
+                
+                if(deliver == cap): break
+            
+            pickup = 0
+            for i in range(p, -1, -1):
+                if(pickup == cap): break
+                
+                if(pickups[i] > 0 and pickup < cap):
+                    if(cap-pickup > deliveries[i]):
+                        pickup += pickups[i]
+                        pickups[i] = 0
+                    else:
+                        pickups[i] -= (cap-pickup)
+                        pickup = cap
+                        
+            print(pickup, deliver)                
+                        
+    
+    return answer*2
